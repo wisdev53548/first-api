d3b54580ec49704a09dc91ffca8e51be92620d59
Tom Smith <wisdev53548@gmail.com>
2017-07-09 20:57:13 -0500

    Added second data set with dummy data

--- server.js
+++ server.js
@@ -1,12 +1,73 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var ingredients = [
+	{
+		'id': '232kAk',
+		'text': 'Eggs'
+	},
+	{
+		'id': 'dkP345',
+		'text': 'Milk'
+	},
+	{
+		'id': 'dkcuu7',
+		'text': 'Bacon'
+	},
+	{
+		'id': '72hdy',
+		'text': 'Frogs Legs'
+	}
+]
+
+var students = [
+	{
+		'First name': 'Ian',
+		'Last name': 'Stone',
+		'Grade': 3
+	},
+	{
+		'First name': 'Michael',
+		'Last name': 'Perry',
+		'Grade': 3
+	},
+	{
+		'First name': 'Tyler',
+		'Last name': 'James',
+		'Grade': 5
+	}
+]
 
 app.get('/', function(req, res) {
-	res.send('My First API!');
+	res.send(ingredients);
 });
 
-app.get('/funions', function(req, res) {
-	res.send('Yo give me some funions foo!');
+app.post('/', function(req, res) {
+	var ingredient = req.body;
+	if (!ingredient || ingredient.text === '') {
+		res.status(500).send({error: 'Your ingredient must have text'})
+	} else {
+		ingredients.push(ingredient);
+		res.status(200).send(ingredients);
+	}
+})
+
+app.get('/students', function(req, res) {
+	res.send(students);
+})
+
+app.post('/students', function(req, res) {
+	var student = req.body;
+	if (!student || student.text === '') {
+		res.status(500).send({error: 'You need to input student information'})
+	} else {
+		students.push(student);
+		res.status(200).send(students);
+	}
 })
 
 app.listen(3000, function() {
